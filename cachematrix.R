628c818c065547a1b064be5d2d08e3187736f62f
14959182 <alina.liman@privatbank.ua>
2015-12-23 16:52:25 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  CI<-NULL
+  set<-function(y) {
+    x<<-y
+    CI<<-NULL
+    }
+  get<-function() x
+  SI<-function(inv) CI<<-inv
+  GI<-function() CI
+  list(set=set,get=get,SI=SI,GI=GI)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  CI<-x$GI()
+  if(!is.null(CI)) {
+    message("getting cached data")
+    return(CI)
+  }
+  data<-x$get()
+  CI<-solve(data,...)
+  x$SI(CI)
+  CI
 }
